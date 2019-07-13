fd4e8c6c4d5fcc68cc126ba3e565faa672f9e631
shion.tanaka <b8219@oic.jp>
2019-07-13 17:34:32 +0900

    create dartfiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 import './top.dart';
+import './reason.dart';
+import './duel.dart';
+import './solve.dart';
+import './complete.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Duel',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         initialRoute: '/',
         routes: <String, WidgetBuilder>{
           '/': (BuildContext context) => new TopPage(),
+          '/reason': (BuildContext context) => null,
+          '/duel': (BuildContext context) => null,
+          '/solve': (BuildContext context) => null,
+          'complete': (BuildContext context) => null,
         });
   }
 }
